24bd561432d365612ec536bbdd0dab0c9b9f018d
Penny520520 <82118825+Penny520520@users.noreply.github.com>
2024-01-03 13:32:36 -0800

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import MailchimpTest2 from './MailchimpTest2'
 import HtmlForm from './HtmlForm'
+import MailchimpTestEmailOnly from './MailchimpTestEmailOnly'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       loadingg
       <MailchimpTest2/>
       {/* <HtmlForm/> */}
+      <MailchimpTestEmailOnly/>
     </div>
   );
 }
