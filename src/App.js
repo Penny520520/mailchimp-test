3eaf704b6a719fe236bbcdaf738ec15d710c88d2
Penny520520 <82118825+Penny520520@users.noreply.github.com>
2024-01-03 13:14:43 -0800

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import MailchimpTest2 from './MailchimpTest2'
+import HtmlForm from './HtmlForm'
 
 function App() {
   return (
     <div className="App">
       loadingg
       <MailchimpTest2/>
+      <HtmlForm/>
     </div>
   );
 }
---
+++ src/HtmlForm/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function HtmlForm() {
+  return (
+    <div>
+      <form action="https://hotmail.us20.list-manage.com/subscribe/post?u=daaf6d61c262407593d7455dd&amp;id=976d627fd9&amp;v_id=4297&amp;f_id=00e953e7f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
+    <div id="mc_embed_signup_scroll">
+        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+        <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""/></div><div class="mc-field-group"><label for="mce-FIRSTNAME">First Name </label><input type="text" name="FIRSTNAME" class=" text" id="mce-FIRSTNAME" value=""/></div><div class="mc-field-group"><label for="mce-LASTNAME">Last Name </label><input type="text" name="LASTNAME" class=" text" id="mce-LASTNAME" value=""/></div>
+<div hidden=""><input type="hidden" name="tags" value="3755573"/></div>
+    <div id="mce-responses" class="clear foot">
+        <div class="response" id="mce-error-response" style="display: none;"></div>
+        <div class="response" id="mce-success-response" style="display: none;"></div>
+    </div>
+    <div class="optionalParent">
+        <div class="clear foot">
+            <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe"/>
+        </div>
+    </div>
+</div>
+</form>
+    </div>
+  )
+}
