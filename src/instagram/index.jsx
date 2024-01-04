202f1d285cdab19df1e5454429c41901f7688333
Penny520520 <82118825+Penny520520@users.noreply.github.com>
2024-01-03 16:53:45 -0800

    update-instagram

--- src/instagram/index.jsx
+++ src/instagram/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import InstagramEmbed from 'react-instagram-embed';
-
+console.log(InstagramEmbed)
 export default function InstagramTest() {
   return (
     <div>
       <InstagramEmbed
-        url='https://instagr.am/p/Zw9o4/'
-        clientAccessToken='123|456'
+        url='https://www.instagram.com/sweethomelitter'
+        clientAccessToken='IGQWROQXFaSWlqUFNKTEFZAbGpfbkZAPMTUycHNMNEZAuUXFpNkc5Y0R4UkdHOHBLSzNQSXpJdW0zV2pYcmdTbHdIT1l5RVg0YW0yazQybzdGdm94ZA2RUOGVKbUQ5U2J0MXl6T1N1WFllMXdSRG1zMVdJdXVQU29Md2cZD'
         maxWidth={320}
         hideCaption={false}
         containerTagName='div'
